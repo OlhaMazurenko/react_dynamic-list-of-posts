0e2b1c3d98e15360902a62ade7ab97576fee76c2
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-06-11 09:16:12 +0300

    Updates

---
+++ precache-manifest.153dce7e5a266cb43bbdaddd93a223eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e447aa97a7b328e36dd5310517f834c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "a18a4dc73b3b274d95f4",
+    "url": "/react_dynamic-list-of-posts/static/css/main.98d3d2c0.chunk.css"
+  },
+  {
+    "revision": "22a965e88c80956f5e2b",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a99c8d40.chunk.js"
+  },
+  {
+    "revision": "a18a4dc73b3b274d95f4",
+    "url": "/react_dynamic-list-of-posts/static/js/main.a7f943dc.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
